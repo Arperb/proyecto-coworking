a0659e48df48db397685f916daf94e33411b9da0
hack18 <esther_h_e@hotmail.com>
2021-03-21 11:12:09 +0100

    añado fieldset a Contacto

--- front/src/Contacto/Contacto.js
+++ front/src/Contacto/Contacto.js
@@ -1,54 +1,56 @@
+import { useState } from "react";
+import "./Contacto.css";
 
-import {useState} from 'react'
-import './Contacto.css'
+function Contacto() {
+  const [sent, setSent] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSent(true);
+  };
 
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [question, setQuestion] = useState("");
 
+  if (sent) {
+    return <div>✉️ ¡Enviado!.Pronto nos pondremos en contacto contigo✉️ </div>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>CONTACTO</h1>
+      <fieldset>
+        <label>
+          Nombre
+          <input
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          Email
+          <input
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>
+          Duda, sugerencia, consulta
+          <input
+            name="question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </label>
+      </fieldset>
+      <button>✉️ Enviar</button>
+    </form>
+  );
+}
 
-
-
-function Contacto(){
-    
-    const [sent, setSent] = useState(false)
-    const handleSubmit = e =>{
-        e.preventDefault()
-            setSent(true)
-        }
-       
-        const [username, setUsername]=useState('')
-        const [email, setEmail] = useState('')
-        const [question, setQuestion] = useState('')
-     
-         
-        
-        if (sent){
-            return(
-                <div>✉️ ¡Enviado!.Pronto nos pondremos en contacto contigo✉️ </div>
-            )
-        }
-        return (
-        <form onSubmit={handleSubmit}>
-         <h1>CONTACTO</h1>  
-            <label>
-                Nombre
-                <input name="username" value={username} onChange={e =>setUsername(e.target.value)}/>
-            </label>
-            <label>
-                Email
-                <input name="email" value={email} onChange={e =>setEmail(e.target.value)} />
-            </label>
-            <label>
-                Duda, sugerencia, consulta
-                <input name="question" value={question} onChange={e =>setQuestion(e.target.value)} />
-            </label>
-            <button>✉️ Enviar</button>
-            
-       
-            
-        </form>
-     
-        )
-       
-    }
-   
-
-export default Contacto
+export default Contacto;
