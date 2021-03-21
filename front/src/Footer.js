2af02e4a1f434591c4d693cdc107d3374f928ecb
jael <nenadepuerto@gmail.com>
2021-03-21 16:18:05 +0100

    añadido de classname al footer para estilo

--- front/src/Footer.js
+++ front/src/Footer.js
@@ -1,57 +1,59 @@
-import { Link } from 'react-router-dom';
-import './Footer.css'
-import logo from './images/logo.png'
-import twitter from './images/twitter.png'
-import facebook from './images/facebook.png'
-import instagram from './images/instagram.png'
-import linkedin from './images/linkedin.png'
-
-
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import logo from "./images/logo.png";
+import twitter from "./images/twitter.png";
+import facebook from "./images/facebook.png";
+import instagram from "./images/instagram.png";
+import linkedin from "./images/linkedin.png";
 
 function Footer() {
-
-    return (
-        <footer className="Footer">
-            <div className = "container1" >
-            <div className="logo">
-            <Link to="/">
-                <img src={logo} alt="Galiking" />
-            </Link>
-            </div>
-            <div className="redes">
-            <a href="https://twitter.com" target="_blank" >
-            <img src={twitter} alt="twitter" />
-            </a>
-            <a href="https://facebook.com" target="_blank" >
-            <img src={facebook} alt="facebook"/>
-            </a>
-            <a href="https://instagram.com" target="_blank" >
-            <img src={instagram} alt="instagram"/>
-            </a>
-            <a href="https://linkedin.com"target="_blank" >
-            <img src={linkedin} alt="linkedin"/>
-            </a>
-            </div>
-            </div>
-            <div className = "container2" >
-            <Link to="/">Coworking</Link>
-            <Link to="/">¿Dónde estamos?</Link>
-            <Link to="/">¿Por qué un coworking?</Link>
-            <Link to="/Registro">Crear Cuenta</Link>
-            <Link to="/Login">Acceso</Link>
-            </div>
-            <div className = "container3" >
-            <h3>Contacto</h3>
-            <h4>contacto@galiking.com</h4>
-            <h4>teléfono:000 000 000</h4>
-            </div>
-            
-
-        </footer>
-    );
+  return (
+    <footer className="Footer">
+      <div className="container1">
+        <div className="logo">
+          <Link to="/">
+            <img className="end logo" src={logo} alt="Galiking" />
+          </Link>
+        </div>
+      </div>
+      <div className="container2">
+        <Link className="finales" to="/">
+          Coworking
+        </Link>
+        <Link className="finales" to="/">
+          ¿Dónde estamos?
+        </Link>
+        <Link className="finales" to="/">
+          ¿Por qué un coworking?
+        </Link>
+        <Link className="finales" to="/Registro">
+          Crear Cuenta
+        </Link>
+        <Link className="finales" to="/Login">
+          Acceso
+        </Link>
+      </div>
+      <div className="container3">
+        <h3>Contacto</h3>
+        <h4>contacto@galiking.com</h4>
+        <h4>teléfono:000 000 000</h4>
+      </div>
+      <div className="redes sociales">
+        <a href="https://twitter.com" target="_blank">
+          <img className="icono media" src={twitter} alt="twitter" />
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <img className="icono media" src={facebook} alt="facebook" />
+        </a>
+        <a href="https://instagram.com" target="_blank">
+          <img className="icono media" src={instagram} alt="instagram" />
+        </a>
+        <a href="https://linkedin.com" target="_blank">
+          <img className="icono media" src={linkedin} alt="linkedin" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
-
-
-
