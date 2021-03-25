9ff09ff77caf143cbae0070c77b1bbb5d91f564d
jael <nenadepuerto@gmail.com>
2021-03-25 22:29:21 +0100

    retirado el fieldset de register.js

--- front/src/Auth/Register.js
+++ front/src/Auth/Register.js
@@ -26,83 +26,76 @@ function Registro() {
     <div>
       <h2>Regístrate</h2>
 
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <input
-            name="email"
-            required
-            placeholder="email..."
-            value={usuario.email || ""}
-            onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="password"
-            type="password"
-            required
-            placeholder="Contraseña..."
-            value={usuario.contrasena || ""}
-            onChange={(e) =>
-              setUsuario({ ...usuario, contrasena: e.target.value })
-            }
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="nif_cif"
-            placeholder="NIF o CIF..."
-            required
-            value={usuario.nif_cif || ""}
-            onChange={(e) =>
-              setUsuario({ ...usuario, nif_cif: e.target.value })
-            }
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="nombre"
-            placeholder="Nombre..."
-            value={usuario.nombre || ""}
-            onChange={(e) => setUsuario({ ...usuario, nombre: e.target.value })}
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="telefono"
-            placeholder="Teléfono..."
-            value={usuario.telefono || ""}
-            onChange={(e) =>
-              setUsuario({ ...usuario, telefono: e.target.value })
-            }
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="bio"
-            placeholder="¿A qué te dedicas?"
-            value={usuario.bio || ""}
-            onChange={(e) => setUsuario({ ...usuario, bio: e.target.value })}
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="rol"
-            placeholder="Si quieres reservar un coworking. Pon: Cliente.
+      <form className="register form" onSubmit={handleSubmit}>
+        <input
+          className="idatos"
+          name="email"
+          required
+          placeholder="email..."
+          value={usuario.email || ""}
+          onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
+        />
+        <br></br>
+        <input
+          className="idatos"
+          name="password"
+          type="password"
+          required
+          placeholder="Contraseña..."
+          value={usuario.contrasena || ""}
+          onChange={(e) =>
+            setUsuario({ ...usuario, contrasena: e.target.value })
+          }
+        />
+        <br></br>
+        <input
+          name="nif_cif"
+          placeholder="NIF o CIF..."
+          required
+          value={usuario.nif_cif || ""}
+          onChange={(e) => setUsuario({ ...usuario, nif_cif: e.target.value })}
+        />
+        <br></br>
+        <input
+          className="idatos"
+          name="nombre"
+          placeholder="Nombre y Apellido"
+          value={usuario.nombre || ""}
+          onChange={(e) => setUsuario({ ...usuario, nombre: e.target.value })}
+        />
+        <br></br>
+        <input
+          name="telefono"
+          placeholder="Teléfono..."
+          value={usuario.telefono || ""}
+          onChange={(e) => setUsuario({ ...usuario, telefono: e.target.value })}
+        />
+        <br></br>
+        <input
+          className="idatos"
+          name="bio"
+          placeholder="¿A qué te dedicas?"
+          value={usuario.bio || ""}
+          onChange={(e) => setUsuario({ ...usuario, bio: e.target.value })}
+        />
+        <br></br>
+        <input
+          className="idatos"
+          name="rol"
+          placeholder="Si quieres reservar un coworking. Pon: Cliente.
                             Si quieres registrar un coworking. Ponr: Propietario"
-            value={usuario.rol || ""}
-            onChange={(e) => setUsuario({ ...usuario, rol: e.target.value })}
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            name="foto"
-            value={usuario.foto || ""}
-            type="file"
-            onChange={(e) => setUsuario({ ...usuario, foto: e.target.value })}
-          />
-        </fieldset>
+          value={usuario.rol || ""}
+          onChange={(e) => setUsuario({ ...usuario, rol: e.target.value })}
+        />
+        <br></br>
+        <input
+          name="foto"
+          value={usuario.foto || ""}
+          type="file"
+          onChange={(e) => setUsuario({ ...usuario, foto: e.target.value })}
+        />
         <button>Quiero coronarme</button>
+
         {error && <div>Ya existe este usuario</div>}
       </form>
     </div>
