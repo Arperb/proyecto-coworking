2fa3859763f4983ddc2e08f31e23cdd004f446d7
jael <nenadepuerto@gmail.com>
2021-03-25 22:08:23 +0100

    correccion de header y home

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,40 +1,52 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import './Header.css'
-import logo from './images/logo.png'
-import Main from './Usuario/Main'
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "./images/logo.png";
+import Main from "./Usuario/Main";
 
 function Header() {
-  const login = useSelector(s => s.login)
+  const login = useSelector((s) => s.login);
 
   //const avatarStyle = login && login.usuario.foto && { backgroundImage: 'url(' + login.usuario.foto + ')' }
 
-
   return (
-    <header className='headerContainer'>
+    <header className="headerContainer">
       <div>
         <Link to="/">
           <img className="logo" src={logo} alt="Galiking" />
         </Link>
       </div>
-      <div className='linksContainer'>
-          <Link className="links" to="/Buscador"><p>Coworkings</p></Link>
-          <Link className="links" to="/Register"><p>Crear Cuenta</p></Link>
-          <Link className="links" to="/Contacto"><p>Contacto</p></Link>
-          {!login &&
-            <Link to="/login">Iniciar sesión</Link>
-        }
-        </div>
-        <div>
-        {login &&
-            <div className="userMenu">
-               <div className="avatar" style={login.usuario.foto && { backgroundImage: `url(http://localhost:9999/images/profile/${login.usuario.foto}.jpg)` }} />
-               <Main>{login.usuario.email}</Main>
-               </div>
-
-      
-            }
-         </div>
+      <div className="linksContainer">
+        <Link className="links" to="/Buscador">
+          <p>Coworkings</p>
+        </Link>
+        <Link className="links" to="/Register">
+          <p>Crear Cuenta</p>
+        </Link>
+        <Link className="links" to="/Contacto">
+          <p>Contacto</p>
+        </Link>
+        {!login && (
+          <Link className="links" to="/login">
+            Iniciar sesión
+          </Link>
+        )}
+      </div>
+      <div>
+        {login && (
+          <div className="userMenu">
+            <div
+              className="avatar"
+              style={
+                login.usuario.foto && {
+                  backgroundImage: `url(http://localhost:9999/images/profile/${login.usuario.foto}.jpg)`,
+                }
+              }
+            />
+            <Main>{login.usuario.email}</Main>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
